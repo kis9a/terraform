2982609539902832b092f597c898293d549d4bd7
kis9a <kis9ax@gmail.com>
2021-10-06 23:39:44 +0900

    fix: acm region with provider alias

--- main.tf
+++ main.tf
@@ -17,6 +17,11 @@ provider "aws" {
   skip_region_validation = true
 }
 
+provider "aws" {
+  alias  = "virginia"
+  region = "us-east-1"
+}
+
 variable "aws_profile" {
   type    = string
   default = "kis9a"
@@ -39,6 +44,7 @@ resource "aws_route53_zone" "kis9b" {
 resource "aws_acm_certificate" "this" {
   domain_name       = "*.${var.domain-kis9b}"
   validation_method = "DNS"
+  provider          = aws.virginia
   tags = {
     Name = var.domain-kis9b
   }
