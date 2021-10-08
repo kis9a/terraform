d4f99fec88fdc03efe9d8a913451990041626c09
kis9a <kis9ax@gmail.com>
2021-10-08 11:21:42 +0900

    feat: update for region

--- main.tf
+++ main.tf
@@ -17,6 +17,19 @@ provider "aws" {
   skip_region_validation = true
 }
 
+provider "aws" {
+  alias   = "virginia"
+  region  = "us-east-1"
+  profile = var.aws_profile
+}
+
+provider "aws" {
+  alias   = "tokyo"
+  region  = "ap-northeast-1"
+  profile = var.aws_profile
+}
+
+# variables
 variable "aws_profile" {
   type    = string
   default = "kis9a"
@@ -36,7 +49,21 @@ resource "aws_route53_zone" "kis9b" {
   name = var.domain-kis9b
 }
 
-resource "aws_acm_certificate" "this" {
+resource "aws_acm_certificate" "virginia-kis9b-subs" {
+  provider          = aws.virginia
+  domain_name       = "*.${var.domain-kis9b}"
+  validation_method = "DNS"
+  tags = {
+    Name = var.domain-kis9b
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate" "tokyo-kis9b-subs" {
+  provider          = aws.tokyo
   domain_name       = "*.${var.domain-kis9b}"
   validation_method = "DNS"
   tags = {
