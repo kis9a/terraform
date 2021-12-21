4d0e1a65b5a6f1fbe24641f23e9d997b2e8bde2e
kis9a <kis9ax@gmail.com>
2021-12-22 08:10:04 +0900

    init iam ecs_deploy

--- iam/iam_policy_attachment.tf
+++ iam/iam_policy_attachment.tf
@@ -7,3 +7,43 @@ resource "aws_iam_user_policy_attachment" "user_frontend_cloudfront_full_access"
   user       = aws_iam_user.frontend.name
   policy_arn = "arn:aws:iam::aws:policy/CloudFrontFullAccess"
 }
+
+resource "aws_iam_user_policy_attachment" "amazon_ecs_task_execution_role_policy" {
+  user       = aws_iam_user.ecs_deploy.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_user_policy_attachment" "secrets_manager_read_write" {
+  user       = aws_iam_user.ecs_deploy.name
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+}
+
+resource "aws_iam_user_policy_attachment" "amazon_elasti_cache_full_access" {
+  user       = aws_iam_user.ecs_deploy.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonElastiCacheFullAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "amazon_s3_full_access" {
+  user       = aws_iam_user.ecs_deploy.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "aws_code_deploy_role_for_ecs" {
+  user       = aws_iam_user.ecs_deploy.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
+}
+
+resource "aws_iam_user_policy_attachment" "amazon_ses_full_access" {
+  user       = aws_iam_user.ecs_deploy.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSESFullAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "amazon_rds_data_full_access" {
+  user       = aws_iam_user.ecs_deploy.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSDataFullAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "amazon_ssm_read_only_access" {
+  user       = aws_iam_user.ecs_deploy.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+}
--- iam/iam_user.tf
+++ iam/iam_user.tf
@@ -1,3 +1,47 @@
+resource "aws_iam_user" "admin" {
+  name = "${var.service}-admin"
+  tags = {
+    Name = "${var.service}-admin"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_iam_user" "maintainer" {
+  name = "${var.service}-maintainer"
+  tags = {
+    Name = "${var.service}-maintainer"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_iam_user" "readonly" {
+  name = "${var.service}-readonly"
+  tags = {
+    Name = "${var.service}-readonly"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_iam_user" "ecs_deploy" {
+  name = "${var.service}-ecs-deploy"
+  tags = {
+    Name = "${var.service}-ecs-deploy"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
 resource "aws_iam_user" "frontend" {
   name = "${var.service}-frontend"
   tags = {
