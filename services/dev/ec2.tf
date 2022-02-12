7cbc1317465e5edec9c56a35a2acb3f8a3fd74a2
kis9a <kis9ax@gmail.com>
2022-02-13 00:02:04 +0900

    services dev ec2 associate public ip

--- services/dev/ec2.tf
+++ services/dev/ec2.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "dev" {
-  count                  = 1
-  subnet_id              = data.terraform_remote_state.network_dev.outputs.dev_subnet_public_ids[0]
-  ami                    = var.dev_ec2_instance_image
-  instance_type          = var.dev_ec2_instance_type
-  key_name               = var.service
-  vpc_security_group_ids = [aws_security_group.dev_ec2_instance.id]
-  iam_instance_profile   = var.dev_ec2_instance_is_ssm_login ? var.ssm_iam_instance_profile : null
+  count                       = 1
+  subnet_id                   = data.terraform_remote_state.network_dev.outputs.dev_subnet_public_ids[0]
+  ami                         = var.dev_ec2_instance_image
+  instance_type               = var.dev_ec2_instance_type
+  key_name                    = var.service
+  vpc_security_group_ids      = [aws_security_group.dev_ec2_instance.id]
+  iam_instance_profile        = var.dev_ec2_instance_is_ssm_login ? var.ssm_iam_instance_profile : null
+  associate_public_ip_address = true
 
   root_block_device {
     volume_type = "gp2"
