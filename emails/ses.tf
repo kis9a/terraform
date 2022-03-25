e30644bd4b9fdc12e95ec5ef5393e002e7c921f4
kis9a <kis9a@users.noreply.github.com>
2022-03-25 09:17:55 +0000

    \[actions] .github/workflows/fmt.yml, terraform fmt -recursive

--- emails/ses.tf
+++ emails/ses.tf
@@ -9,18 +9,18 @@ resource "aws_ses_domain_dkim" "dkim" {
 }
 
 resource "aws_ses_domain_mail_from" "ses" {
-  provider = aws.virginia
+  provider         = aws.virginia
   domain           = aws_ses_domain_identity.ses.domain
   mail_from_domain = "mail.${aws_ses_domain_identity.ses.domain}"
   depends_on       = [aws_ses_domain_identity.ses]
 }
 
 resource "aws_ses_domain_identity" "ses_tokyo" {
-  domain   = data.terraform_remote_state.domains.outputs.domain_kis9a
+  domain = data.terraform_remote_state.domains.outputs.domain_kis9a
 }
 
 resource "aws_ses_domain_dkim" "dkim_tokyo" {
-  domain   = aws_ses_domain_identity.ses_tokyo.domain
+  domain = aws_ses_domain_identity.ses_tokyo.domain
 }
 
 resource "aws_ses_domain_mail_from" "ses_tokyo" {
