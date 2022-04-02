23d7de919561bea9e33e68a119a0f0407307b3e9
kis9a <kis9ax@gmail.com>
2022-04-02 23:05:49 +0900

    feat: init oro TXT record

--- domains/route53.tf
+++ domains/route53.tf
@@ -5,3 +5,11 @@ resource "aws_route53_zone" "kis9b" {
 resource "aws_route53_zone" "kis9a" {
   name = var.domain_kis9a
 }
+
+resource "aws_route53_record" "txt_orero" {
+  zone_id = aws_route53_zone.kis9a.id
+  name    = "oro.${var.domain_kis9a}"
+  type    = "TXT"
+  ttl     = "60"
+  records = [base64encode(file("${path.module}/oro"))]
+}
