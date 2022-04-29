c09792c9a79ef6ac2895c4511dba383ef886b081
kis9a <kis9ax@gmail.com>
2022-04-29 22:25:58 +0900

    init bastion dev service

---
+++ services/bastion/dev/bastion.tf
@@ -0,0 +1,9 @@
+module "bastion" {
+  source        = "../../../modules/bastion"
+  env           = var.env
+  service       = var.service
+  subnet_ids    = data.terraform_remote_state.network.outputs.dev_subnet_app_ids
+  vpc_id        = data.terraform_remote_state.network.outputs.dev_vpc_id
+  ecr_image_url = "${data.terraform_remote_state.ecr.outputs.bastion_repository_url}:v1.0"
+  ecs_cluster_id = data.terraform_remote_state.ecs.outputs.cluster_dev.arn
+}
---
+++ services/bastion/dev/outputs.tf
@@ -0,0 +1,3 @@
+output "bastion_container_definition_json" {
+  value = module.bastion.bastion_container_definition_json
+}
