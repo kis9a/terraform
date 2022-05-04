44b0292eb29512e326daae163b86f2d2a5675b32
kis9a <kis9a@users.noreply.github.com>
2022-05-04 09:20:18 +0000

    \[actions] .github/workflows/fmt.yml, terraform fmt -recursive

--- components/waf/outputs.tf
+++ components/waf/outputs.tf
@@ -3,5 +3,5 @@ output "managed_rule_arn" {
 }
 
 output "cloudfront_basic_authentication" {
-  value =  aws_wafv2_web_acl.cloudfront_basic_authentication.arn
+  value = aws_wafv2_web_acl.cloudfront_basic_authentication.arn
 }
