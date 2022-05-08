53c29acfab591928832f4d0fdbb707d5029a0937
kis9a <kis9ax@gmail.com>
2022-05-08 18:55:37 +0900

    add TXT record for keybase_nav_kis9a_proof_of_identify

--- components/domains/route53.tf
+++ components/domains/route53.tf
@@ -13,3 +13,11 @@ resource "aws_route53_record" "txt_orero" {
   ttl     = "60"
   records = [base64encode(file("${path.module}/oro"))]
 }
+
+resource "aws_route53_record" "keybase_nav_kis9a_proof_of_identify" {
+  zone_id = aws_route53_zone.kis9a.id
+  name    = "nav.${var.domain_kis9a}"
+  type    = "TXT"
+  ttl     = "60"
+  records = ["keybase-site-verification=sPrCeSQdd8ZXWenCe21Lc3Wb5UGwGSQcq1DV_-5XLfw"]
+}
