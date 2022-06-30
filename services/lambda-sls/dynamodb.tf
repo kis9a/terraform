abc4884c63a7555403b2fe055fa0695e4af40b8c
kis9a <kis9ax@gmail.com>
2022-07-01 06:34:54 +0900

    add dynamodb table for remo

--- services/lambda-sls/dynamodb.tf
+++ services/lambda-sls/dynamodb.tf
@@ -21,3 +21,33 @@ resource "aws_lambda_event_source_mapping" "this" {
   starting_position = "TRIM_HORIZON"
   depends_on        = [aws_dynamodb_table.this, aws_iam_policy.dynamodb]
 }
+
+resource "aws_dynamodb_table" "remo" {
+  name           = "lambda-sls-remo"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "id"
+  range_key      = "unixtime"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "unixtime"
+    type = "N"
+  }
+
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+}
+
+resource "aws_lambda_event_source_mapping" "remo" {
+  function_name     = aws_lambda_function.this.arn
+  event_source_arn  = aws_dynamodb_table.remo.stream_arn
+  batch_size        = 100
+  enabled           = true
+  starting_position = "TRIM_HORIZON"
+  depends_on        = [aws_dynamodb_table.remo, aws_iam_policy.dynamodb]
+}
--- services/lambda-sls/lambda.tf
+++ services/lambda-sls/lambda.tf
@@ -1,3 +1,14 @@
+data "aws_kms_secrets" "secrets" {
+  secret {
+    name    = "secrets"
+    payload = file("${path.module}/secrets.yaml.encrypted")
+  }
+}
+
+locals {
+  s = yamldecode(data.aws_kms_secrets.secrets.plaintext["secrets"])
+}
+
 resource "aws_lambda_function" "this" {
   function_name = var.service
   role          = aws_iam_role.this.arn
@@ -12,11 +23,21 @@ resource "aws_lambda_function" "this" {
       ENDPOINT       = "lambda"
       S3_TODO_BUCKET = aws_s3_bucket.images.id
       DB_TODO_TABLE  = aws_dynamodb_table.this.id
+      DB_REMO_TABLE  = aws_dynamodb_table.remo.id
+      REMO_API_TOKEN = local.s.remo_api_token
     }
   }
 }
 
 resource "aws_lambda_permission" "this" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.this.execution_arn}/*/*/*"
+}
+
+resource "aws_lambda_permission" "remo" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.this.function_name
